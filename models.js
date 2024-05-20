0b885ef75169f6f8667700aeb3b99444a4ad44f0
ElodineOfficial <139085761+ElodineOfficial@users.noreply.github.com>
2024-05-20 12:29:51 -0400

    Added context limits

--- models.js
+++ models.js
@@ -1,117 +1,45 @@
-const models = {
-claude: {
-  endpoint: 'YOUR PROXY URL',
-  apiKeyField: 'x-api-key',
-  apiKeyPrefix: '',
-  messageField: 'prompt',
-  requestBody: {
-    model: 'claude-v1',
-    max_tokens_to_sample: 1024,
-    stop_sequences: ['\n\nHuman:', '\n\nAssistant:'],
+// modelSettings.js
+const modelSettings = {
+  claude: {
+    fields: [
+      { name: 'top_p', type: 'number', default: null, step: 0.1, min: 0, max: 1 },
+      { name: 'top_k', type: 'number', default: null, step: 1, min: 0 },
+      { name: 'max_tokens_to_sample', type: 'number', default: 1024, step: 1, min: 1 },
+      { name: 'temperature', type: 'number', default: 1, step: 0.1, min: 0, max: 1 },
+      {
+        name: 'model',
+        type: 'select',
+        default: 'claude-v1',
+        options: ['claude-v1', 'claude-2.1', 'claude-2.0', 'claude-instant-1.2', 'claude-3-opus-20240229', 'claude-3-sonnet-20240229', 'claude-3-haiku-20240307'],
+      },
+    ],
   },
-  headers: {
-    'anthropic-version': '2023-06-01',
-  },
-  extractResponseContent: (data) => {
-    if (data) {
-      if (data.completion) {
-        return data.completion;
-      } else if (data.content && data.content.length > 0) {
-        const textContent = data.content.find(item => item.type === 'text');
-        if (textContent) {
-          return textContent.text;
-        }
-      }
-    }
-    return null;
-  },
-  formatConversationHistory: (conversationHistory, characterName, initialMessage) => {
-    let formattedHistory = '';
-
-    // Start with the initial message if present
-    if (initialMessage) {
-      formattedHistory += `\n\nAssistant: ${initialMessage}\n\nHuman: ${
-        conversationHistory[0]?.content || "Hello! I'm excited to start our conversation."
-      }`;
-    } else {
-      formattedHistory += `\n\nHuman: ${
-        conversationHistory[0]?.content || "Hello! I'm excited to start our conversation."
-      }`;
-    }
-
-    // Append the rest of the conversation history
-    formattedHistory += conversationHistory.slice(1).map((message) => {
-      return message.role === 'user' ? `\n\nHuman: ${message.content}` : `\n\nAssistant: ${message.content}`;
-    }).join('');
-
-    // Ensure to prompt for the next assistant's message if the last message was from the user
-    const lastMessage = conversationHistory[conversationHistory.length - 1];
-    if (lastMessage && lastMessage.role !== 'assistant') {
-      formattedHistory += '\n\nAssistant:';
-    }
-
-    return formattedHistory;
-  },
-},
-
   gpt: {
-    endpoint: 'https://api.openai.com/v1/chat/completions',
-    apiKeyField: 'Authorization',
-    apiKeyPrefix: 'Bearer ',
-    messageField: 'messages',
-    requestBody: {
-      model: 'gpt-3.5-turbo',
-    },
-    extractResponseContent: (data) => {
-      if (data.choices && data.choices.length > 0) {
-        return data.choices[0].message.content;
-      }
-      return null;
-    },
-    formatConversationHistory: (conversationHistory,) => {
-      return [
-        { role: 'system', content: `I am ready to begin whenever you are. Please offer instructions as you see fit.` },
-        ...conversationHistory,
-      ];
-    },
+    fields: [
+      { name: 'temperature', type: 'number', default: .8, step: 0.1, min: 0, max: 1 },
+      { name: 'presence_penalty', type: 'number', default: 1.6, step: 0.1, min: -2, max: 2 },
+      { name: 'max_tokens', type: 'number', default: 1024, step: 1, min: 1 },
+      { name: 'frequency_penalty', type: 'number', default: 1.8, step: 0.1, min: -2, max: 2 },
+	  {
+        name: 'model',
+        type: 'select',
+        default: 'gpt-3.5-turbo',
+        options: ['gpt-4', 'gpt-4o', 'gpt-4-turbo', 'gpt-4-turbo-2024-04-09', 'gpt-4-turbo-preview', 'gpt-4-0125-preview', 'gpt-4-1106-preview', 'gpt-4-0613', 'gpt-3.5-turbo-0125', 'gpt-3.5-turbo', 'gpt-3.5-turbo-1106', 'gpt-3.5-turbo-16k', 'gpt-3.5-turbo-0613', 'gpt-3.5-turbo-16k-0613'],
+      },
+    ],
   },
- cohere: {
-    endpoint: 'https://api.cohere.ai/v1/chat',
-    apiKeyField: 'Authorization',
-    apiKeyPrefix: 'Bearer ',
-    messageField: 'message',
-    requestBody: {
-      model: 'command-r-plus',
-      max_tokens: 250,
-      temperature: 0.7,
-    },
-    headers: {
-      accept: 'application/json',
-      'content-type': 'application/json',
-    },
-    extractResponseContent: (data) => {
-      if (data.generations && data.generations.length > 0) {
-        return data.generations[0].text;
-      }
-      return null;
-    },
-formatConversationHistory: (conversationHistory) => {
-  const cleanMessage = (message) => {
-    return message.replace(/^\d+\n\n/, '').replace(/\n\n\d+$/, '').replace(/\n/g, ' ').trim();
-  };
-
-  const formattedHistory = conversationHistory.map((message) => ({
-    role: message.role === 'user' ? 'USER' : 'CHATBOT',
-    message: cleanMessage(message.content),
-  }));
-
-  return {
-    chat_history: formattedHistory,
-    message: cleanMessage(conversationHistory[conversationHistory.length - 1].content),
-    connectors: [{ id: 'web-search' }],
-  };
-},
+  cohere: {
+    fields: [
+      { name: 'temperature', type: 'number', default: 0.75, step: 0.1, min: 0, max: 1 },
+      { name: 'max_tokens', type: 'number', default: 250, step: 1, min: 1 },
+      { 
+        name: 'model', 
+        type: 'select',
+        default: 'command-r-plus',
+        options: ['command-r-plus'],
+      },
+    ],
   },
 };
 
-export default models;
+export default modelSettings;
